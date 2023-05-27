70467dad0ad7f3f94694e815020a97c9d51c696c
danthecm <etchiedaniel1@gmail.com>
2023-05-27 12:00:51 +0100

    feat: created stats endpoint for viewing table statictics

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,22 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {
+    "amenities": Amenity,
+    "cities": City,
+    "places": Place,
+    "reviews": Review,
+    "states": State,
+    "users": User
+}
 
 
 @app_views.route("/status")
@@ -11,3 +27,11 @@ def status():
     """Return the status the api."""
     ans = {'status': 'OK'}
     return jsonify(ans)
+
+
+@app_views.route("/stats")
+def stats():
+    """Returns the stats of all tables."""
+    storage.reload()
+    stats = {k: storage.count(v) for k, v in classes.items()}
+    return jsonify(stats)

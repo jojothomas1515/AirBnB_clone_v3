8ef42f2a6acfcb7836a6a48b9b0862fe47c49fc2
Jojo <jojothomas1515@gmail.com>
2023-05-27 19:52:03 +0100

    implemented state route to support 4 methods [GET, POST, PUT, DELETE]

--- exp_env.sh
+++ exp_env.sh
@@ -2,5 +2,5 @@ export HBNB_MYSQL_USER=hbnb_dev
 export HBNB_MYSQL_PWD=hbnb_dev_pwd
 export HBNB_MYSQL_HOST=localhost
 export HBNB_MYSQL_DB=hbnb_dev_db
-export HBNB_TYPE_STORAGE=file
-export HBNB_ENV=test
+export HBNB_TYPE_STORAGE=db
+export HBNB_ENV=dev

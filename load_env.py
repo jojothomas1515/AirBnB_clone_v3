111749e86899602dbfc9e5f1a7d2fb524f421f71
danthecm <etchiedaniel1@gmail.com>
2023-05-28 17:34:18 +0100

    feat: handle possible errors in load_env function"

--- load_env.py
+++ load_env.py
@@ -5,9 +5,12 @@ import os
 
 def load_env_file():
     """Loads all environment variables from .env file"""
-    with open('.env', 'r') as file:
-        for line in file:
-            line = line.strip()
-            if line and not line.startswith('#'):
-                key, value = line.split('=', 1)
-                os.environ[key] = value
+    try:
+        with open('.env', 'r') as file:
+            for line in file:
+                line = line.strip()
+                if line and not line.startswith('#'):
+                    key, value = line.split('=', 1)
+                    os.environ[key] = value
+    except Exception:
+        pass

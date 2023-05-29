5c74d1233b309c5ecdef47be74824c3c3d9e121b
Jojo <jojothomas1515@gmail.com>
2023-05-29 14:46:19 +0100

    Debugging v3 cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -93,7 +93,7 @@ def update_city(city_id: str):
 
     if not city:
         abort(404)
-    data = request.get_json()
+    data = request.get_json(silent=True)
     if data is None:
         return jsonify({"error": "Not a JSON"}), 400
     for k, v in data.items():
@@ -106,7 +106,7 @@ def update_city(city_id: str):
 
 
 @app_views.route('/states/<string:state_id>/cities', strict_slashes=False,
-                 methods=["GET", "POST"])
+                 methods=["GET"])
 def state_cities(state_id: str):
     """
     Cities of a State
@@ -120,30 +120,42 @@ def state_cities(state_id: str):
            associated with.
     """
 
-    if request.method == "GET":
-        state = storage.get(State, state_id)
-        if not state:
-            abort(404)
-        cities = list(
-            map(lambda city: city.to_dict(),
-                state.cities)
-            )
-        return jsonify(cities), 200
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    cities = list(
+        map(lambda city: city.to_dict(),
+            state.cities)
+    )
+    return jsonify(cities), 200
 
-    elif request.method == "POST":
-        state = storage.get(State, state_id)
 
-        if not state:
-            abort(404)
-        try:
-            data = request.get_json()
+@app_views.route('/states/<string:state_id>/cities', strict_slashes=False,
+                 methods=["POST"])
+def add_cities(state_id: str):
+    """
+    Cities of a State
 
-            if 'name' not in data:
-                return jsonify(error="Missing name"), 400
-            data['state_id'] = state_id
-            city = City(**data)
-            city.save()
+    GET: retrieve all the cities associated with a state on the storage
 
-            return jsonify(city.to_dict()), 201
-        except BadRequest:
-            return jsonify(error="Not a JSON"), 400
+    POST: add a city to the state referenced by state_id
+
+    Args:
+       state_id: id  of the state the retrieved or added city is
+           associated with.
+    """
+
+    state = storage.get(State, state_id)
+
+    if not state:
+        abort(404)
+
+    data = request.get_json(silent=True)
+    if data is None:
+        return jsonify(error="Not a JSON"), 400
+    if 'name' not in data:
+        return jsonify(error="Missing name"), 400
+    data['state_id'] = state_id
+    city = City(**data)
+    city.save()
+    return jsonify(city.to_dict()), 201

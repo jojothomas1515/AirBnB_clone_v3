afd3bc46cb76ce8fe705c8ec30838e6233453aae
Jojo <jojothomas1515@gmail.com>
2023-05-29 14:18:36 +0100

    Debugging v3 cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -11,8 +11,8 @@ from werkzeug.exceptions import BadRequest
 
 
 @app_views.route("/cities/<string:city_id>", strict_slashes=False,
-                 methods=["DELETE", "GET", "PUT"])
-def cities(city_id: str):
+                 methods=["GET"])
+def get_city(city_id: str):
     """City routes.
 
     This route accepts GET, POST, PUT, DELETE http methods
@@ -31,37 +31,76 @@ def cities(city_id: str):
     Args:
         city_id: id of the cities resource
     """
-    if request.method == "GET":
-        city = storage.get(City, city_id)
-        return (jsonify(city.to_dict()), 200) if city else abort(404)
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    return jsonify(city.to_dict()), 200
 
-    elif request.method == "DELETE":
-        city = storage.get(City, city_id)
-        if not city:
-            return abort(404)
-        city.delete()
-        storage.save()
-        return jsonify({}), 200
+@app_views.route("/cities/<string:city_id>", strict_slashes=False,
+                 methods=["DELETE"])
+def delete_city(city_id: str):
+    """City routes.
 
-    elif request.method == "PUT":
-        ignore = ['id', 'created_at', 'updated_at']
-        try:
-            city = storage.get(City, city_id)
+    This route accepts GET, POST, PUT, DELETE http methods
 
-            if not city:
-                abort(404)
-            else:
-                data = request.get_json()
+    GET: is use to get the city resources from the database,
+    if an id is passed to it the it gets a single city object matching
+    the city id passed. If it is unable to find the city it return a 404
+    status code of not found.
 
-                for k, v in data.items():
-                    if k in ignore:
-                        continue
-                    setattr(city, k, v)
+    DELETE: this method deleete object matching it id from the storage in use,
+    this method requires the city id
 
-                city.save()
-                return jsonify(city.to_dict()), 200
-        except BadRequest:
-            return jsonify(error="Not a JSON"), 400
+    PUT: this method is use to update resource or object already in the storage
+    in use, and it requires the city id
+
+    Args:
+        city_id: id of the cities resource
+    """
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+    city.delete()
+    storage.save()
+    return jsonify({}), 200
+
+@app_views.route("/cities/<string:city_id>", strict_slashes=False,
+                 methods=["PUT"])
+def update_city(city_id: str):
+    """City routes.
+
+    This route accepts GET, POST, PUT, DELETE http methods
+
+    GET: is use to get the city resources from the database,
+    if an id is passed to it the it gets a single city object matching
+    the city id passed. If it is unable to find the city it return a 404
+    status code of not found.
+
+    DELETE: this method deleete object matching it id from the storage in use,
+    this method requires the city id
+
+    PUT: this method is use to update resource or object already in the storage
+    in use, and it requires the city id
+
+    Args:
+        city_id: id of the cities resource
+    """
+
+    ignore = ['id', 'created_at', 'updated_at']
+    city = storage.get(City, city_id)
+
+    if not city:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        return jsonify({"error": "Not a JSON"}) 400
+    for k, v in data.items():
+        if k in ignore:
+            continue
+        setattr(city, k, v)
+
+    city.save()
+    return jsonify(city.to_dict()), 200
 
 
 @app_views.route('/states/<string:state_id>/cities', strict_slashes=False,
